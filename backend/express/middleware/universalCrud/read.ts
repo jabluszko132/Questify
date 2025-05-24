476af81c38312dfbbea419afd36f80191dc2d9a3
Jan Stanisław Nowacki <123495481+jabluszko132@users.noreply.github.com>
2025-05-24 18:04:05 +0200

    add middleware

--- backend/express/middleware/universalCrud/read.ts
+++ backend/express/middleware/universalCrud/read.ts
@@ -26,3 +26,30 @@ export function getReadByIdParamMiddleware(idName: string, prismaSchema: any, is
         }
     }
 }
+
+export function getReadAllByParamMiddleware(idName: string, prismaSchema: any, isString?: boolean) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            if(!(idName in req.params)) {
+                res.status(400).end("Bad request")
+                return
+            }
+            prismaSchema.findMany({
+                where: {
+                    [idName]: isString ? req.params[idName] : parseInt(req.params[idName])
+                }
+            }).then((result: any) => {
+                if(result.length > 0){
+                    res.status(200).end(JSON.stringify(result))
+                }else{
+                    res.status(404).end(JSON.stringify({ "404": "Not found" }));
+                }
+                return
+            }).catch((err: any) => {
+                next(err)
+            })
+        } catch (err) {
+            next(err)
+        }
+    }
+}
