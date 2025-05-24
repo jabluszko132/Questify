1c4338714c9185319e276e8953df2ba51ee52b8e
Jan Stanisław Nowacki <123495481+jabluszko132@users.noreply.github.com>
2025-05-24 19:57:40 +0200

    remove unused file

--- backend/express/utils/keyCheck.ts
+++
@@ -1,8 +0,0 @@
-function implementsClass(obj: any, cls: any): boolean {
-  if (!obj) {
-    return false;
-  }
-  return Object.getOwnPropertyNames(cls).every(x => { obj.keys().includes(x) })
-}
-
-export default implementsClass;
