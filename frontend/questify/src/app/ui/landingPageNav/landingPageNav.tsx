318509fb17d789d783cdd2176f839a7f5547d248
6Danio6 <danio@3lance.pl>
2025-05-26 03:29:18 +0200

    add logo to nav

--- frontend/questify/src/app/ui/landingPageNav/landingPageNav.tsx
+++ frontend/questify/src/app/ui/landingPageNav/landingPageNav.tsx
@@ -1,24 +1,40 @@
 import styles from "./landingPageNav.module.css";
 import Link from "next/link";
 
+import Image from "next/image";
+
 export default function LandingPageNav() {
   return (
     <nav className={styles.nav}>
-      <Link href="/" className={styles.link}>
-        Home
-      </Link>
-      <Link href="/login" className={styles.link}>
-        Login
-      </Link>
-      <Link href="/register" className={styles.link}>
-        Register
-      </Link>
-      <Link href="/about" className={styles.link}>
-        About us
-      </Link>
-      <Link href="/contact" className={styles.link}>
-        Contact
-      </Link>
+      <div className={styles.logoContainer}>
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Questify Logo"
+            width={48}
+            height={48}
+            className={styles.logo}
+            priority
+          />
+        </Link>
+      </div>
+      <div className={styles.links}>
+        <Link href="/" className={styles.link}>
+          Home
+        </Link>
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
+        <Link href="/register" className={styles.link}>
+          Register
+        </Link>
+        <Link href="/about" className={styles.link}>
+          About us
+        </Link>
+        <Link href="/contact" className={styles.link}>
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 }
