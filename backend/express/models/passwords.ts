055ee82fa68f256e72b3d13b2b5494fb010faaba
6Danio6 <danio@3lance.pl>
2025-05-26 11:09:57 +0200

    remove salt column from passwords, fix schemas

--- backend/express/models/passwords.ts
+++ backend/express/models/passwords.ts
@@ -2,11 +2,9 @@ import { z } from "zod";
 
 export const PasswordsSchema = z.object({
     user_id: z.number(),
-    password: z.string(),
-    salt: z.string()
+    hash: z.string()
 });
 
 export const PasswordsReq = z.object({
-    password: z.string(),
-    salt: z.string()
+    hash: z.string()
 });
--- backend/express/models/users.ts
+++ backend/express/models/users.ts
@@ -1,12 +1,15 @@
 import { z } from 'zod';
+import { PasswordsSchema } from './passwords';
 
 export const UsersSchema = z.object({
     id: z.number(),
     nickname: z.string(),
-    email: z.string()
+    email: z.string(),
+    password: PasswordsSchema
 });
 
 export const UsersReq = z.object({
     nickname: z.string(),
-    email: z.string()
+    email: z.string(),
+    password: z.string()
 });
